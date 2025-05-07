53ff9c255f94c5c5d11b89e6539333e19b71cdfa
Brisma Alvarez Valdez <147790770+brbrbre@users.noreply.github.com>
2025-05-07 16:38:16 -0700

    Update CameraUpper.py

--- vision/src/LARC/CameraUpper.py
+++ vision/src/LARC/CameraUpper.py
@@ -6,11 +6,11 @@ import time
 class CameraUpper:
     def __init__(self, port=None, camera_index=0, min_area=3800, max_area=50000, max_diameter=250):
         self.colors = {
-            "Red": ([0, 129, 40], [17, 255, 255]),
-            "Blue": ([99, 114, 141], [112, 255, 249]),
-            "Orange": ([2, 142, 200], [19, 250, 255]),
-            "Yellow": ([23, 76, 123], [33, 216, 197]),
-            "Black": ([0, 0, 0], [180, 255, 34]),
+            "Red": ([0, 92, 98], [12, 255, 255]),
+            "Blue": ([90, 88, 159], [146, 255, 255]),
+            "Orange": ([0, 56, 102], [42, 255, 255]),
+            "Yellow": ([25, 52, 67], [40, 255, 255]),
+            "Black": ([0, 0, 0], [180, 65, 107]),
         }
         self.color_bgr_values = {
             "Red": (0, 0, 255),
